dd59bbbbc9824d4f16e6f35f0eb63dacc3971a34
DriesDD <driesdedecker@gmail.com>
2020-08-13 15:41:15 +0200

    started over, with cards and without bulma

--- weather.js
+++ weather.js
@@ -1,4 +1,5 @@
-let url, city, weatherdata, photodata, tarray, tfeelsarray, iconarray, timearray, hoursarray, now
+let url, city, weatherdata, photodata, tarray, tfeelsarray, iconarray, timearray, hoursarray, now,
+    tarray1, tarray2, tarray3, tarray4, tarray5
 
 //to do
 //break up into days, cards
@@ -19,71 +20,111 @@ document.getElementById('inputcity').oninput = () => {
 }
 
 async function update() {
-    url = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city +     '&mode=xmly&appid=48a8b4741cc8cb086ca2bbffc8c983cb'
+    url = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&mode=xmly&appid=48a8b4741cc8cb086ca2bbffc8c983cb'
     tempweatherdata = await getData(url)
     if (tempweatherdata.city != undefined) {
         weatherdata = tempweatherdata
         console.log(weatherdata.list)
-        //updatebackground()
+        updatebackground()
 
-        tarray = []; timearray = []; tfeelsarray = []; iconarray = []; hoursarray = [];
+        //make arrays of all the needed data points
+        tarray = [];
+        timearray = [];
+        tfeelsarray = [];
+        iconarray = [];
+        hoursarray = [];
+        graphmin = 30;
+        graphmax = 0;
         let day;
         for (i = 0; i < weatherdata.list.length; i++) {
             timearray.push(Date(weatherdata.list[i].dt_txt))
             let day = new Date(weatherdata.list[i].dt_txt)
-            hoursarray.push(day.getHours())
-            tarray.push(0.1 * Math.round((weatherdata.list[i].main.temp - 273.15) * 10))
-            tfeelsarray.push(0.1 * Math.round((weatherdata.list[i].main.feels_like - 273.15) * 10))
-            iconarray.push("http://openweathermap.org/img/w/"+weatherdata.list[i].weather[0].icon+".png");
-
+            hoursarray.push(day.getHours() + "h")
+            tarray.push(Math.round(weatherdata.list[i].main.temp - 273.15))
+            tfeelsarray.push(Math.round(weatherdata.list[i].main.feels_like - 273.15))
+            iconarray.push("http://openweathermap.org/img/w/" + weatherdata.list[i].weather[0].icon + ".png");
+            //add the standardized minima and maxima which are the same for each day chart
+            graphmin = Math.min(graphmin, tarray[i])
+            graphmax = Math.max(graphmax, tarray[i])
         }
+        graphmin = Math.floor((graphmin - 1) / 5) * 5
+        graphmax = Math.ceil((graphmax + 1) / 5) * 5
+        console.log(graphmin, graphmax)
 
+        //for the first day, add the missing hours that have already passed to the front
         now = new Date(weatherdata.list[0].dt_txt)
-        console.log(now.getHours()/3)
-        for (i=0; i<now.getHours()/3; i++)
-        {timearray.unshift(i)
-         hoursarray.unshift(i)
-         tarray.unshift(i)
-         tfeelsarray.unshift(i)
-         iconarray.unshift(i)
+        console.log(now.getHours() / 3)
+        for (i = 1; i < now.getHours() / 3; i++) {
+            timearray.unshift(null)
+            hoursarray.unshift(now.getHours() - i * 3 + "h")
+            tarray.unshift(null)
+            tfeelsarray.unshift(null)
+            iconarray.unshift(null)
         }
+        //now break up the array in five arrays of one day each
+        tarray1 = tarray.slice(0, 9);
+        tarray2 = tarray.slice(8, 17);
+        tarray3 = tarray.slice(16, 25);
+        tarray4 = tarray.slice(24, 33);
+        tarray5 = tarray.slice(32, 41);
+        hoursarray1 = hoursarray.slice(0, 9);
 
+        Chart.defaults.global.defaultFontSize = '20';
+        Chart.defaults.global.defaultFontFamily = "'Pangolin',  cursive;";
 
-        //now the actual table
-        new Chart(document.getElementById("mixed-chart"), {
-            responsive: true,
-            maintainAspectRatio: false,
-            aspectRatio: 1,
-            type: 'bar',
-            data: {
-                labels: hoursarray,
-                datasets: [{
-                    label: "Temperature in °C",
-                    type: "line",
-                    borderColor: "#fa0",
-                    borderWidth: 5,
-                    data: tarray,
-                    pointStyle: 'https://openweathermap.org/img/w/10d.png',
-                    lineTension: 0.4,
-                    fill: false
-                }, {
-                    label: "Feels like °C",
-                    type: "line",
-                    borderColor: "#f20",
-                    data: tfeelsarray,
-                    fill: false
-                }]
-            },
-            options: {
-                title: {
-                    display: false,
-                    text: 'Weather'
+        //now the tables
+        for (i = 1; i <= 5; i++) {
+            new Chart(document.getElementById("chart" + i), {
+                type: 'line',
+                data: {
+                    labels: hoursarray1,
+                    datasets: [{
+                        label: "Temperature in °C",
+                        type: "line",
+                        borderColor: "#fa0",
+                        borderWidth: 5,
+                        data: eval('tarray'+i),
+                        pointStyle: 'https://openweathermap.org/img/w/10d.png',
+                        lineTension: 0.4,
+                        fill: false
+                    }]
                 },
-                legend: {
-                    display: true
+                options: {
+                    scales: {
+                        yAxes: [{
+                            display: false,
+                            ticks: {
+                                min: graphmin,
+                                max: graphmax
+                            }
+                        }],
+                        xAxes: [{
+                            drawBorder: false
+                        }],
+
+                    },
+                    tooltips: {
+                        enabled: false
+                    },
+                    title: {
+                        display: false,
+                        text: 'Weather'
+                    },
+                    legend: {
+                        display: false
+                    },
+                    plugins: {
+                        datalabels: {
+                            color: '#fa0',
+                            offset: 'top',
+                            clamp: true,
+                            backgroundColor: '#000'
+                        }
+                    }
                 }
-            }
-        });
+
+            });
+        }
 
     }
 }
@@ -94,7 +135,7 @@ async function updatebackground() {
 
     photodata = await getData('https://api.unsplash.com/photos/random?client_id=p2RsPWAavBwh-hvyW9GzFInfh3S3PC7W7VnLkTG6wVo&query=' + city + '%20sky&orientation=landscape')
     console.log(photodata.urls.full)
-    document.getElementById("hero").style = "background-image: url(" + photodata.urls.regular + ");background-position: center;background-size: cover"
+    document.getElementById("background").style = "background-image: url(" + photodata.urls.regular + ");background-position: center;background-size: cover"
 }
 
 /*
